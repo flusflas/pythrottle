01883744fdfbc71fcd695f71120ee66fc8c2929f
Flus Flas <aflusflas@gmail.com>
2019-10-26 12:14:57 +0200

    Remove semaphore

--- src/metronome.py
+++ src/metronome.py
@@ -3,7 +3,7 @@ from time import perf_counter
 
 
 class Metronome:
-    def __init__(self, interval=None, auto_reset=True, exact=False, loop=None):
+    def __init__(self, interval=None, auto_reset=True, exact=False):
         self.interval = interval
         self.auto_reset = auto_reset
         self.exact = exact
@@ -11,7 +11,6 @@ class Metronome:
         self.timer = None
         self.available = 0
         self.ticks = 0
-        self.sem = asyncio.Semaphore(value=1, loop=loop)
         self.started = False
         self.start()
 
@@ -38,8 +37,7 @@ class Metronome:
 
     async def wait_until_available(self, seconds=None):
         seconds = self._get_interval(seconds)
-        async with self.sem:    # TODO: Semaphore seems not necessary...
-            # self.ticks += 1
-            aux = (self.t_start + seconds)
-            self.t_start += seconds
-            await asyncio.sleep(aux - perf_counter())
+        t_target = (self.t_start + seconds)
+        self.t_start += seconds
+        await asyncio.sleep(t_target - perf_counter())
+        self.ticks += 1
