30d27ca53fd449bc2e000fa76256d9cc01c30855
Flus Flas <aflusflas@gmail.com>
2019-11-01 11:07:34 +0100

    Add profiler fixture

--- src/tests/test_metronome.py
+++ src/tests/test_metronome.py
@@ -18,24 +18,29 @@ def metronome():
     yield Metronome(interval=(1 / RATE))
 
 
+@pytest.fixture
+def profiler():
+    iter_count = TESTS_DURATION * RATE
+    profiler = Profiler(iter_count, target_rate=RATE)
+    yield profiler
+
+
 def test_interval_missing():
     met = Metronome()
     with pytest.raises(ValueError):
         met.elapsed()
 
 
-def test_sync_elapsed_exact(met):
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
+def test_sync_elapsed_exact(met, profiler):
+    with profiler:
         sleep_time = (0.01 / RATE) if (0.01 / RATE) < 0.001 else 0.001
-        for i in range(iter_count):
+        for i in range(profiler.iter_count):
             while not met.elapsed(exact=True):
                 time.sleep(sleep_time)
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
+    assert met.ticks == profiler.iter_count
 
 
 def test_sync_elapsed_inexact():
@@ -55,62 +60,56 @@ def test_sync_elapsed_inexact():
     assert met.ticks == iter_count
 
 
-def test_sync_sleep(met):
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in range(iter_count):
+def test_sync_sleep(met, profiler):
+    with profiler:
+        for i in range(profiler.iter_count):
             met.sleep_until_available()
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
+    assert met.ticks == profiler.iter_count
 
 
-def test_sync_sleep_loop(met):
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in met.sleep_loop(max_ticks=iter_count):
+def test_sync_sleep_loop(met, profiler):
+    with profiler:
+        for i in met.sleep_loop(max_ticks=profiler.iter_count):
             pass
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
-    assert i == iter_count
+    assert met.ticks == profiler.iter_count
+    assert i == profiler.iter_count
 
 
-def test_restart(met):
+def test_restart(met, profiler):
     """
     Tests the behavior of a single Metronome instance iterating during two
     periods of time separate separated by a short sleep. After this break,
     the Metronome is restarted to check that the behavior is the same in
     the two periods.
     """
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in met.sleep_loop(max_ticks=iter_count):
+    with profiler:
+        for i in met.sleep_loop(max_ticks=profiler.iter_count):
             pass
 
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
-    assert i == iter_count
+    assert met.ticks == profiler.iter_count
+    assert i == profiler.iter_count
 
     time.sleep(1)
     met.restart()
 
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in met.sleep_loop(max_ticks=iter_count):
+    with profiler:
+        for i in met.sleep_loop(max_ticks=profiler.iter_count):
             pass
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
-    assert i == iter_count
+    assert met.ticks == profiler.iter_count
+    assert i == profiler.iter_count
 
 
-def test_no_restart(met):
+def test_no_restart(met, profiler):
     """
     Tests the behavior of a single Metronome instance iterating during two
     periods of time separate separated by a short sleep. After this break,
@@ -118,58 +117,53 @@ def test_no_restart(met):
     be shorter as the Metronome tries to reach its internal rate, and
     therefore the metrics in this period should be different.
     """
-    iter_count = TESTS_DURATION * RATE
     rest_time = 1
 
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in met.sleep_loop(max_ticks=iter_count):
+    with profiler:
+        for i in met.sleep_loop(max_ticks=profiler.iter_count):
             pass
 
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
-    assert i == iter_count
+    assert met.ticks == profiler.iter_count
+    assert i == profiler.iter_count
 
     time.sleep(rest_time)
 
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in met.sleep_loop(max_ticks=iter_count):
+    with profiler:
+        for i in met.sleep_loop(max_ticks=profiler.iter_count):
             pass
 
-    expected_rate = iter_count / (TESTS_DURATION - rest_time)
+    expected_rate = profiler.iter_count / (TESTS_DURATION - rest_time)
     error = 1 - profiler.measured_rate / expected_rate
 
     print(f"Rate: {profiler.measured_rate}, Expected Rate: {expected_rate}, Error: {100 * error:.3f}%")
     assert abs(error) < MAX_ERROR
-    assert met.ticks == 2 * iter_count
-    assert i == iter_count
+    assert met.ticks == 2 * profiler.iter_count
+    assert i == profiler.iter_count
 
 
 @pytest.mark.asyncio
-async def test_async_wait(met):
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        for i in range(iter_count):
+async def test_async_wait(met, profiler):
+    with profiler:
+        for i in range(profiler.iter_count):
             await met.wait_until_available()
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
+    assert met.ticks == profiler.iter_count
 
 
 @pytest.mark.asyncio
-async def test_async_wait_tasks(met):
-    iter_count = TESTS_DURATION * RATE
-
+async def test_async_wait_tasks(met, profiler):
     async def aux_task(m: Metronome):
         await m.wait_until_available()
 
     # Split async tasks in chunks to avoid large number of tasks in the loop
     # (which reduce performace for high rates and distorts the test)
-    remaining = iter_count
+    remaining = profiler.iter_count
     max_tasks = 100
 
-    with Profiler(iter_count, target_rate=RATE) as profiler:
+    with profiler:
         while remaining > 0:
             size = remaining if remaining < max_tasks else max_tasks
             remaining -= size
@@ -177,18 +171,16 @@ async def test_async_wait_tasks(met):
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
+    assert met.ticks == profiler.iter_count
 
 
 @pytest.mark.asyncio
-async def test_async_wait_loop(met):
-    iter_count = TESTS_DURATION * RATE
-
-    with Profiler(iter_count, target_rate=RATE) as profiler:
-        async for i in met.wait_loop(max_ticks=iter_count):
+async def test_async_wait_loop(met, profiler):
+    with profiler:
+        async for i in met.wait_loop(max_ticks=profiler.iter_count):
             pass
 
     print(f"Rate: {profiler.measured_rate}, Error: {100 * profiler.error:.3f}%")
     assert abs(profiler.error) < MAX_ERROR
-    assert met.ticks == iter_count
-    assert i == iter_count
+    assert met.ticks == profiler.iter_count
+    assert i == profiler.iter_count
